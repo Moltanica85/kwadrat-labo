b7ba304d91ea8845e8285b80339892e77148c620
Moltanica85 <133493046+Moltanica85@users.noreply.github.com>
2026-05-07 23:25:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/4kwadrat-labo/', // <--- TUTAJ DODAJESZ TĘ LINIJKĘ
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
